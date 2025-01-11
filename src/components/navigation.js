63416b09ef1dc7dd0066cc8b657538407370a70d
kaungsett <rrocket438@gmail.com>
2025-01-11 14:17:06 +0630

    click issue fix

--- src/components/navigation.js
+++ src/components/navigation.js
@@ -66,7 +66,7 @@ export default function Navigation() {
               :  "mx-1 bg-secondary border-2 text-egw border-white  px-4 py-2 rounded-full"
            }`}
           >
-            <Link href={item2.path}>
+            <Link href={item2.path} onClick={() => setIsOpen(false)}>
               <h1 className="block font-bold">{item2.name}</h1>
             </Link>
           </li>
